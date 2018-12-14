b4de82eca17ede94f1eec9f38050f2df7880e285
stephbak <stephane.bakum@sfr.fr>
2018-12-14 16:01:28 +0100

    exercice3

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+  <meta charset="utf-8" />
+  <title>php partie1</title>
+</head>
+<body>
+  <p>
+    <?php
+    $km = 1;
+    echo $km;
+    ?>
+  </p>
+  <p>
+    <?php
+    $km = 3;
+    echo $km;
+    ?>
+  </p>
+  <p>
+    <?php
+    $km = 125;
+    echo $km;
+    ?>
+  </p>
+</body>
+</html>
